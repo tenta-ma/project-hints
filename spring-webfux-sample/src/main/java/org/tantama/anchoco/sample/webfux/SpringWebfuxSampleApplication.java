ff4cf739a2b968a444c04c5f1d1af9b5ecea5e42
tenta-ma <shintani.manabu@gmail.com>
2022-05-26 16:07:51 +0900

    initial

---
+++ spring-webfux-sample/src/main/java/org/tantama/anchoco/sample/webfux/SpringWebfuxSampleApplication.java
@@ -0,0 +1,13 @@
+package org.tantama.anchoco.sample.webfux;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringWebfuxSampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringWebfuxSampleApplication.class, args);
+	}
+
+}
---
+++ spring-webfux-sample/src/test/java/org/tantama/anchoco/sample/webfux/SpringWebfuxSampleApplicationTests.java
@@ -0,0 +1,13 @@
+package org.tantama.anchoco.sample.webfux;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringWebfuxSampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
